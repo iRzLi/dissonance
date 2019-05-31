cb2ad0ad1df4cb3a0338efe95967b2be8c6d78e1
Run Zhuo Li <runzhuoli.cs@gmail.com>
2019-05-30 22:08:26 -0400

    alter footer distance

--- frontend/components/splash/splash_footer.jsx
+++ frontend/components/splash/splash_footer.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 const splashFooter = props => {
     return (
         <div className="splash-footer">
-            <span>
-                <div className="footer-text">
+            <div className="splash-footer-section">
+                <ul className="footer-text">
                     <h1>Ready to try Discord? It's free!</h1>
                     <span>JOIN OVER 250 MILLION PLAYERS TODAY</span>
-                </div>
-                <div className="signupdiv">
+                </ul>
+                <ul className="signupdiv">
                     <Link to="/register" className="signupButton">Sign Up Now</Link>
-                </div>
-            </span>
+                </ul>
+            </div>
         </div>
     );
 };
