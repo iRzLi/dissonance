ffee6c99912e0d39e778e01f4322d76aad20abc1
Run Zhuo Li <runzhuoli.cs@gmail.com>
2019-06-27 09:44:02 -0400

    add route protection

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -10,12 +10,12 @@ import IndexContainer from './index/index_container';
 const App = () => (
     <>
         <Modal />
+        <Switch>
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/register" component={SignupFormContainer} />
-        <Route exact path="/" component={SplashComponent} />
-        <Switch>
         <ProtectedRoute exact path="/channel/:serverId" component={IndexContainer} />
         <ProtectedRoute exact path="/channel/:serverId/:channelId"  component={IndexContainer}/>
+        <Route path="/" component={SplashComponent} />
         </Switch>
     </>
 );
