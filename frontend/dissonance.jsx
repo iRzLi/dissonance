dab99a2f713109e7a0e160448295c965f3432bc4
Run Zhuo Li <runzhuoli.cs@gmail.com>
2019-06-27 09:31:05 -0400

    remove store from window

--- frontend/dissonance.jsx
+++ frontend/dissonance.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import * as PRUtil from './util/private_room_api_util';
-import * as PMUtil from './util/private_message_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     let store;
-    // console.log(window.currentUser);
     if (window.currentUser) {
         const preloadedState = {
             entities: {
@@ -21,9 +18,5 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         store = configureStore();
     }
-
-    window.store = store;
-    window.PRUtil = PRUtil;
-    window.PMUtil = PMUtil;
     ReactDOM.render(<Root store={store} />, root);
 });
