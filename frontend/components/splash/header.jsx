47752d130cb392e351aeff58d4ee5e3dfaa9ba40
Run Zhuo Li <runzhuoli.cs@gmail.com>
2019-07-25 17:41:50 -0400

    add links to angellist and linkedin

--- frontend/components/splash/header.jsx
+++ frontend/components/splash/header.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import LoginLogoutContainer from '../session/login_logout_container';
 const header = (props) => {
 
     let goToChat = null;
-    if (props.loggedIn){
+    if (props.loggedIn) {
         goToChat = (<span className="login-link" onClick={(e) => {
             props.history.push("/channel/me")
         }} >Open</span>)
     }
     return (
         <>
-        <nav className="header">
-            <ul>
-                <li className="logo"><i className="fab fa-discord"></i> Dissonance</li>
-            </ul>
-            <ul>
-                <ul className="header-icons">
-                        <li><a href="https://github.com/iRzLi/dissonance"><i className="fab fa-github"></i></a></li>
+            <nav className="header">
+                <ul>
+                    <li className="logo"><i className="fab fa-discord"></i> Dissonance</li>
                 </ul>
+                <ul>
+                    <ul className="header-icons">
+                        <li><a href="https://angel.co/run-zhuo-li"><i class="fab fa-angellist"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/run-zhuo-li/"><i class="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="https://github.com/iRzLi/dissonance"><i className="fab fa-github"></i></a></li>
+                    </ul>
                     <li>{goToChat}</li>
                     <li><LoginLogoutContainer /></li>
-            </ul>
-        </nav>
-        <nav className="small-header">
-            <div className="inner-small-header">
-                <span className="logo"><i className="fab fa-discord"></i>Dissonance</span>
-                <span className="bars-logo"><i className="fas fa-bars"></i></span>
-            </div>
-            
-            {/* <ul className="header-dropdown">
+                </ul>
+            </nav>
+            <nav className="small-header">
+                <div className="inner-small-header">
+                    <span className="logo"><i className="fab fa-discord"></i>Dissonance</span>
+                    <span className="bars-logo"><i className="fas fa-bars"></i></span>
+                </div>
+
+                {/* <ul className="header-dropdown">
                 <li>Link1</li>
                 <li>Link2</li>
                 <li>Link3</li>
             </ul> */}
-        </nav>
+            </nav>
         </>
     );
 };
